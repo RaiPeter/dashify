74dc7d002e4d1988b6c570e6a0cbd2a62f0eb950
Peter Rai <peter.r@smarter.codes>
2025-02-28 16:01:19 +0530

    added css for property details

--- frontend/src/PropertyDetails.jsx
+++ frontend/src/PropertyDetails.jsx
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PropertyDetails.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/";
 import {
   faPhone,
   faEnvelope,
   faLocationDot,
-  faPenToSquare
+  faPenToSquare,
+  faArrowUpFromBracket,
+  faHouse,
+  faKaaba,
+  faHeart,
+  faPersonSwimming,
+  faLeaf,
+  faDoorOpen,
+  faVideo,
+  faSquareParking,
+  faDumbbell,
+  faSnowflake,
+  faFire,
 } from "@fortawesome/free-solid-svg-icons";
 import { useParams } from "react-router";
-// import AgentsImage from "./assets/"
+import PropertyImage from "./assets/sdvsr.webp";
+import axiosInstance from "./interceptor/interceptor";
+
+const iconMap = {
+  Clubhouse :  faKaaba,
+  "Fitness Center" : faHeart,
+  "Swimming Pool" : faPersonSwimming,
+  Gym: faDumbbell,
+  Garden : faLeaf,
+  "AC & Heating": faSnowflake,
+  Fireplace: faFire,
+  Balcony : faDoorOpen,
+  "Security System" : faVideo,
+  Parking : faSquareParking,
+};
 
 const PropertyDetails = () => {
   const params = useParams();
+  const [features, setFeautres] = useState([])
+
+  useEffect(() => {
+    const fetchProperty = async () =>{
+      try{
+        const response = await axiosInstance.get(
+          `/api/properties?${params.toString()}`
+        );
+        console.log("heasdfasdfasd");
+      }catch(e){
+        console.error(`Error fetching ${params.name} property details`, e);
+      }
+    }
+    fetchProperty();
+  },[params])
+
   return (
     <div className="property-details">
       <div className="left">
@@ -56,33 +98,46 @@ const PropertyDetails = () => {
       <div className="right">
         <div className="property">
           <div className="image">
-            <img src="" alt="propery image" />
+            <img src={PropertyImage} alt="propery image" />
           </div>
           <div className="header">
             <div>
               <h3>Property Name</h3>
-              <h4>Property Price</h4>
+              <h4>345,454,3.</h4>
             </div>
             <div>
               <div>
-                <p>Washington, USA</p>
-                <p>Office</p>
+                <p>
+                  <FontAwesomeIcon icon={faLocationDot} /> Washington, USA
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faHouse} />
+                  Office
+                </p>
               </div>
               <div>
-                <button>share</button>
+                <button>
+                  <FontAwesomeIcon icon={faArrowUpFromBracket} /> share
+                </button>
               </div>
             </div>
           </div>
           <div className="features">
-            <div>
-              <h3>Facility</h3>
-              <div>features</div>
+            <h3>Facility</h3>
+            <div className="list">
+              {features.map((feature, index) => (
+                <div key={index} className="feature-item">
+                  <FontAwesomeIcon
+                    icon={iconMap[feature]}
+                  />
+                  <p>{feature}</p>
+                </div>
+              ))}
             </div>
           </div>
           <div className="details">
-            <p>Details & features</p>
-            <p>Details & features</p>
-            <p>Details & features</p>
+            <h3>Details & features</h3>
+            <pre>Details</pre>
           </div>
         </div>
       </div>
